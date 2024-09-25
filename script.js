ee260aaf73503f3e8560c7fe41e9637326e5396c
EZEQUIEL FELICIANO DOS SANTOS <ezequiel.fsantos10@senacsp.edu.br>
2024-09-25 14:57:12 -0300

    primeiro comentario

---
+++ script.js
@@ -0,0 +1,10 @@
+// Solicitar a idade do usuário
+
+let idade = parseInt(prompt("Insira sua idade"));
+
+// Verificar se a idade é maior que 18
+ if (idade >=18) {
+     alert("Acesso permitido! 😉"); 
+    }else{
+        alert("Acesso negado! 😐")
+    }
